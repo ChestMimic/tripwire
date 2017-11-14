f048329b0910b4dcb0a9e1574df49f259c0b9d73
ibbolia <ibbolia@gmail.com>
2017-11-14 15:30:24 -0500

    Minor cleanup

--- tripwire.cpp
+++ tripwire.cpp
@@ -1,7 +1,9 @@
+//tripwire.cpp
+//Run a command when a given process ID terminates
+//Author: Mark Fitzgibbon
 
 //gcc -o tripwire.exe tripwire.cpp -lsapi -lstdc++
 #include <windows.h>
-
 #include <tlhelp32.h>
 #include <iostream>
 #include <string>
@@ -10,7 +12,6 @@
 #include <tchar.h>
 
 bool isProcessIDActive(DWORD processID){
-
 	DWORD aProcesses[1024], cbNeeded, cProcesses;
 	unsigned int i;
 
@@ -27,47 +28,28 @@ bool isProcessIDActive(DWORD processID){
 			}
 		}	
 	}
-
 	return FALSE;
 }
 
 
 int main(int argc, char* argv[]){
-
 	DWORD pidU = -1;
-
 	if (argc > 1){
 		pidU = atoi(argv[1]);
 	}
-
-	//DWORD pid =   GetCurrentProcessId();
-	//printf("%u",pid);
-
-	
     bool trigger = isProcessIDActive(pidU);
-
-    //printf(trigger ? "TRUE" : "FALSE" );
-
     if(trigger){
     	printf("Arming tripwire to Process ID: %d\n.", pidU);
-
     	while(trigger){
-
     		trigger = isProcessIDActive(pidU);
     		Sleep(1000);
     		printf("PING\n");
     	}
     	printf("PID no longer available, triggering followup.");
     	system("shutdown /r /t 60");
-
     }
     else{
-
     	printf("No matching PID to attach tripwire: %d\n", pidU);
     }
-
-
-
-
 	return 0;
 }
